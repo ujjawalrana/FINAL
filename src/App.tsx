7891ee45a5cc562be5f9c0ce19b884ef1c0d0f52
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2023-06-22 02:19:32 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './feature/signIn/pages';
+import bingeBox from '../../../assets/bingeBox.jpg'
+import SignOut from './feature/signOut/pages';
+
+function App() {
+  return (
+   <SignOut />
+  );
+}
+
+export default App;
